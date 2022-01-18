99dbcf703b7be3f23066266e9d29e4005602b815
Civitasv <hscivitasv@gmail.com>
2022-01-18 20:59:02 +0800

    Latest

--- init.el
+++ init.el
@@ -571,10 +571,7 @@
     ;;(setq explicit-zsh-args '())         ;; Use 'explicit-<shell>-args for shell-specific args
 
     ;; Match the default Bash shell prompt.  Update this if you have a custom prompt
-    (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *"))
-
-  (use-package eterm-256color
-    :hook (term-mode . eterm-256color-mode)))
+    (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *")))
 
 (when (eq system-type 'windows-nt)
   (setq explicit-shell-file-name "powershell.exe")
