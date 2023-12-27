33cfd86d63140013ad611f13f286f4bdca61cf08
Civitasv <hscivitasv@gmail.com>
2023-12-27 19:08:46 +0800

    add term support

--- init.el
+++ init.el
@@ -4,4 +4,5 @@
 (cv/load "~/.emacs.d/bootstrap.el")
 (cv/load "~/.emacs.d/helper.el")
 (cv/load "~/.emacs.d/options.el")
+(cv/load "~/.emacs.d/term.el")
 (cv/load "~/.emacs.d/plugins/init.el")
---
+++ term.el
@@ -0,0 +1,7 @@
+(use-package term
+  :config
+  (setq explicit-shell-file-name "zsh") ;; Change this to zsh, etc
+  ;;(setq explicit-zsh-args '())         ;; Use 'explicit-<shell>-args for shell-specific args
+
+  ;; Match the default Bash shell prompt.  Update this if you have a custom prompt
+  (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *"))
